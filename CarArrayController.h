89ded3c840707dd3d8544da3d750c5af05a6f08e
Chris Mear <chris@feedmechocolate.com>
2009-06-05 14:55:22 +0100

    Set date purchased to today's date automatically for new cars. (End of chapter 11.3.)

---
+++ CarArrayController.h
@@ -0,0 +1,16 @@
+//
+//  CarArrayController.h
+//  CarLot
+//
+//  Created by Chris Mear on 05/06/2009.
+//  Copyright 2009 Greenvoice. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CarArrayController : NSArrayController {
+
+}
+
+@end
