0fcb4b5ad8d64f9cf777ff56e4ab715694a12118
Chris Mear <chris@feedmechocolate.com>
2009-07-12 20:30:01 +0100

    First pass of ImageFun.

---
+++ StretchView.h
@@ -0,0 +1,16 @@
+//
+//  StretchView.h
+//  ImageFun
+//
+//  Created by Chris Mear on 12/07/2009.
+//  Copyright 2009 Greenvoice. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface StretchView : NSView {
+
+}
+
+@end
